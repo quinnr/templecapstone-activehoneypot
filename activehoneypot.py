ca664f9e19d0951c2670e5a490994e09e5890e4a
Quinn <quinn.heath@temple.edu>
2018-03-13 04:31:54 -0400

    Update code so the server works, can't accept connections yet but it does listen on the correct port.

--- activehoneypot.py
+++ activehoneypot.py
@@ -1,26 +1,51 @@
 from twisted.conch.ssh import factory
 from twisted.conch.ssh.transport import SSHServerTransport
 from twisted.conch import recvline
+from twisted.internet import reactor
+from twisted.conch.ssh.keys import Key
+from twisted.cred.portal import Portal
 
-print("This is a test.")
+print("Starting SSH server.")
+
+print("Opening public and private key files.")
+with open('id_rsa') as privateKeyFile:
+    privateKeyText = privateKeyFile.read()
+    privateKey = Key.fromString(data=privateKeyText)
+
+with open('id_rsa.pub') as publicKeyFile:
+    publicKeyText = publicKeyFile.read()
+    publicKey = Key.fromString(data=publicKeyText)
 
 
+# Todo: Implement actual portal code
+class HoneypotPortal(Portal):
+    pass
+
+
+# Todo: Implement factory code
 class HoneypotFactory(factory.SSHFactory):
-    protocol = SSHServerTransport
+    privateKeys = {'ssh-rsa': privateKey}
+    publicKeys = {'ssh-rsa': publicKey}
+    portal = Portal(None)  # This should later be changed to HoneypotPortal instead of none once that class is ready.
 
-class InputOutputProtocol(recvline.HistoricRecvLine):
-    def __init__(self, user):
-        self.user = user
 
-    def connectionMade(self):
-        recvline.HistoricRecvLine.connectionMade(self)
-        self.initializeScreen(self)
-        self.terminal.write("This is a test")
-        self.terminal.nextLine()
-        self.showPrompt()
+reactor.listenTCP(2222, HoneypotFactory())  # Open TCP port using specified factory to handle connections.
+reactor.run()
 
-    def showPrompt(self):
-        self.terminal.write("> ")
-
-    def lineReceived(self, line):
-        line = line.strip()
+# TODO: Implement the Protocol basics
+# class InputOutputProtocol(recvline.HistoricRecvLine):
+#     def __init__(self, user):
+#         self.user = user
+#
+#     def connectionMade(self):
+#         recvline.HistoricRecvLine.connectionMade(self)
+#         self.initializeScreen(self)
+#         self.terminal.write("This is a test")
+#         self.terminal.nextLine()
+#         self.showPrompt()
+#
+#     def showPrompt(self):
+#         self.terminal.write("> ")
+#
+#     def lineReceived(self, line):
+#         line = line.strip()
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Make sure to install python-twisted package with 'sudo apt-get install python-twisted'
+Use Python 3.5
+
+Will not run without id_rsa and id_rsa.pub private and public keyfiles in the same directory.
+You can generate these with openssh-keygen.
