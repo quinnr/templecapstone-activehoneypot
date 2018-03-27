de38f712c917b7a89bb9e24de8191ccbd4a4d64b
chrismalantonio <cmalantonio96@hotmail.com>
2018-03-27 00:54:15 -0400

    fixed web api to work with updated database tables

--- activehoneypot_web/src/java/model/attacker/StringData.java
+++ activehoneypot_web/src/java/model/attacker/StringData.java
@@ -5,11 +5,16 @@ public class StringData {
     public String attackerID = "";
     public String ip_address = "";
     public String username = "";
-    public String password = "";
-    public String dateAccessed = "";
-    public String timeOfDayAccessed = "";
-    public String metadata = "";
+    public String passwords = "";
+    public String time_of_day_accessed = "";
     public String logFile = "";
+    public String sessions = "";
+    public String country = "";
+    public String city = "";
+    public String state = "";
+    public String logged_in = "";
+    public String uploaded_files = "";
+    public String date_accessed = "";
     public String errorMsg = "";
 
 }
