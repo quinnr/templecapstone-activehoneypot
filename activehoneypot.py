6547bbbe83dad9c785518340b944d236436211fb
Quinn <quinn.heath@temple.edu>
2018-04-23 02:46:52 -0400

    Add cat command

--- activehoneypot.py
+++ activehoneypot.py
@@ -32,7 +32,7 @@ PRIMES = {
             889633836007296066695655481732069270550615298858522362356462966213994239650370532015908457586090329628589149803446849742862797136176274424808060302038380613106889959709419621954145635974564549892775660764058259799708313210328185716628794220535928019146593583870799700485371067763221569331286080322409646297706526831155237865417316423347898948704639476720848300063714856669054591377356454148165856508207919637875509861384449885655015865507939009502778968273879766962650318328175030623861285062331536562421699321671967257712201155508206384317725827233614202768771922547552398179887571989441353862786163421248709273143039795776049771538894478454203924099450796009937772259125621285287516787494652132525370682385152735699722849980820612370907638783461523042813880757771177423192559299945620284730833939896871200164312605489165789501830061187517738930123242873304901483476323853308396428713114053429620808491032573674192385488925866607192870249619437027459456991431298313382204980988971292641217854130156830941801474940667736066881036980286520892090232096545650051755799297658390763820738295370567143697617670291263734710392873823956589171067167839738896249891955689437111486748587887718882564384870583135509339695096218451174112035938859)],
 }
 
-SUPPORTED_COMMANDS = ["ls", "df", "ifconfig", "uname", "wget", "exit", "shutdown", "whoami", "pwd", "cd", "rm", "rmdir"]
+SUPPORTED_COMMANDS = ["ls", "df", "ifconfig", "uname", "wget", "exit", "shutdown", "whoami", "pwd", "cd", "rm", "rmdir", "cat"]
 
 
 log.startLogging(sys.stderr)
@@ -127,6 +127,8 @@ class HoneypotProtocol(protocol.Protocol):  # Contains functions for handling in
                 self.rm_command(arguments)
             elif command == "rmdir":
                 self.rmdir_command(arguments)
+            elif command == "cat":
+                self.cat_command(arguments)
             #elif command == "passwd":
             #    self.passwd_command(arguments)
             else:
@@ -204,6 +206,29 @@ class HoneypotProtocol(protocol.Protocol):  # Contains functions for handling in
                 return
         self.sendLine("bash: cd: "+ string_dir +": No such file or directory.")
         return
+
+    def cat_command(self, arguments=[]):
+        if not arguments:
+            self.sendLine("cat: missing operand")
+            self.sendLine("Try 'cat --help' for more information.")
+            return
+        else:
+            string_dir = arguments[0]
+            if not string_dir[:1] == "/":
+                string_dir = self.working_directory + "/" + string_dir
+            if self.filesys.filesys.isdir(string_dir):
+                self.sendLine("cat: " + string_dir + ": Is a directory")
+                return
+            if not self.filesys.filesys.isfile(string_dir):
+                self.sendLine("cat: " + string_dir + ": File does not exist")
+                return
+            else:
+                pointer = self.filesys.filesys.open(string_dir, "r")
+                for line in pointer:
+                    self.sendLine(line)
+                pointer.close()
+                return
+            return
 
 
     def whoami_command(self, arguments=[]):
